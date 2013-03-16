3dfeae43cbd648e0009e3aced5ac1425b452603d
Yat Choi <yat.choi@lyriqal.com>
2013-03-16 15:23:25 -0400

    Build word hash

--- lib/facebooker.rb
+++ lib/facebooker.rb
@@ -4,6 +4,8 @@ require 'koala'
 module Sinatra
 	module Facebooker
 		$accessToken = "";
+		$wordHash = Hash.new
+		$wordHash.default = 0
 
 		def bar(name)
 			"#{name}bar"
@@ -13,11 +15,19 @@ module Sinatra
 			@rest = Koala::Facebook::API.new($accessToken)
 
 			feed = @rest.fql_query("select message from status where uid=me()")
+			parseFeed(feed)
 			feed
 		end
 
 		def parseFeed(feed)
-
+			feed.each do |f|
+				tokens = f["message"].split(" ")
+				tokens.each do |t|
+					t.downcase!
+					$wordHash[t] += 1
+				end
+			end
+			puts $wordHash
 		end
 	end
 
